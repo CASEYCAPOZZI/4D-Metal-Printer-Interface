6d8f2f43bd05285197d26b5945bf6c83afbfcd42
Casey Capozzi <caseycapozzi@ymail.com>
2018-06-04 19:15:44 -0500

    Added name to hello

--- src/menards/com/Main.java
+++ src/menards/com/Main.java
@@ -5,7 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Hello World!!");
+		String FName  = "Casey";
+		String LName = "Capozzi";
+		
+
+		System.out.println("Hello " + FName + " " + LName + "!!");
 	}
 
 }
