44e05185a9faa2416a9e216e5f24e8658907f880
Casey Capozzi <caseycapozzi@ymail.com>
2018-06-04 19:23:16 -0500

    added getters and setters

--- src/menards/com/Person.java
+++ src/menards/com/Person.java
@@ -5,4 +5,26 @@ public class Person {
 	private String FName;
 	private String Lname;
 	private static String Employer;
+	
+	
+	public String getFName() {
+		return FName;
+	}
+	public void setFName(String fName) {
+		FName = fName;
+	}
+	public String getLname() {
+		return Lname;
+	}
+	public void setLname(String lname) {
+		Lname = lname;
+	}
+	public static String getEmployer() {
+		return Employer;
+	}
+	public static void setEmployer(String employer) {
+		Employer = employer;
+	}
+	
+	
 }
